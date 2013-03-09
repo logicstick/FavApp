3ef252e45718c46c87bef6fec41c63f8ea4e26b4
Vishwas Sharma <vishwasinventor@gmail.com>
2013-03-10 03:47:44 +0530

    Checking the changes in CEP Example

---
+++ MyApp/MyAppResources.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace FavApp.MyApp
+{
+    class MyAppResources : CefSharp.ISchemeHandler
+    {
+        public bool ProcessRequest(CefSharp.IRequest request, ref string mimeType, ref System.IO.Stream stream)
+        {
+            if (request.Url.StartsWith("bq://") || request.Url.ToString().StartsWith("sp://") || !request.Url.ToString().Contains("://"))
+            { 
+            }
+            return false;
+        }
+    }
+}
